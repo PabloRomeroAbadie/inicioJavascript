5736d116e52b4f35b8c6248bd8b74a84125e71b9
Pablo Romero Abadie <pabloabadie1995@gmail.com>
2021-09-27 11:05:51 -0300

    Primer commit

---
+++ js/1-holamundo.js
@@ -0,0 +1,35 @@
+//esto es un comentario
+
+/* comentario 
+de multiples
+ lineas */
+
+//mostrar un mensaje en la pantalla
+document.write("hola mundo");
+
+//mostrar un mensaje en la consola//
+console.log("hola mundo");
+
+//mostrar un mensaje en una ventana emergente//
+//alert("hola comision 18i");
+
+//crear una variable u otro termino seria declarar una variable o inicializar una variable (var-let-const)//
+let persona = "Axel Campo";
+
+document.write("<br>Persona: "+ persona);
+
+//modificar el valor de una variable
+persona = "Agus Lorenzetti";
+document.write("<br>Persona: "+ persona);
+
+let edad = 23;
+document.write("<br>Edad: "+ edad);
+
+//crear variables constantes
+const URL = "https://campus.rollingcodeschool.com/login/index.php"
+
+document.write("<br>Link: "+ URL);
+
+
+
+
