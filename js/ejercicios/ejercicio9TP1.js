011c276e06159f43bad3531a0a777403c7aa92f9
Pablo Romero Abadie <pabloabadie1995@gmail.com>
2021-10-05 09:42:53 -0300

    realizamos ejercicios de bucles

---
+++ js/ejercicios/ejercicio9TP1.js
@@ -0,0 +1,9 @@
+let frase = prompt("ingrese una frase");
+
+console.log(frase);
+console.log(frase.length);
+console.log(frase.charAt(1));
+
+//recorrer la frase con un bucle, el fin de mi bucle frase.length
+
+//
