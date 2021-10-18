76e0cb1c1d6671138c68a9c82b435931a7064c90
Pablo Romero Abadie <pabloabadie1995@gmail.com>
2021-10-18 10:14:11 -0300

    modifique el archivo de arreglos con funcion

--- js/5-arreglos.js
+++ js/5-arreglos.js
@@ -10,13 +10,11 @@ document.write(series[0]);
 console.log(series.length)
 
 //recorrer elemento por elemento un arreglo
-for(let posicion = 0; posicion < series.length; posicion++){
-    document.write("<br>" +series[posicion]);
-}
+
+mostrarArreglo("recorrer elemento por elemento un arreglo")
 
 //agregar un elemento al final de un arreglo
 series.push("breaking bad");
-
 
 mostrarArreglo("Arreglos series con nuevo elemento")
 
@@ -28,9 +26,7 @@ mostrarArreglo("agregar un elemento en una posicion particular")
 //eliminar un elemento del arreglo
 series.splice(5, 1)
 
-
 mostrarArreglo("eliminar un elemento de una posicion particular")
-
 
 //modificar un valor del arreglo
 series[4] = "friends";
